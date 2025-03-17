35c015d52d31023e688ca26fe431c3a53345f6f8
wuyujin <3075385315@qq.com>
2025-03-17 13:15:47 +0800

    feat

--- next.config.js
+++ next.config.js
@@ -3,7 +3,17 @@ const nextConfig = {
   output: 'standalone',
   webpack: (config) => {
     config.resolve.alias.canvas = false;
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      path: false,
+      stream: false,
+      canvas: false
+    };
     return config;
+  },
+  experimental: {
+    serverActions: true
   }
 }
 
